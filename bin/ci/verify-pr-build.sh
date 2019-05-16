2d5e8c470d7bb0f6a181daaac90888a73ccfdae4
Scott Macfarlane <smacfarlane@chef.io>
2019-05-16 09:59:45 -0700

    disable pipefail
    Signed-off-by: Scott Macfarlane <smacfarlane@chef.io>

--- bin/ci/verify-pr-build.sh
+++ bin/ci/verify-pr-build.sh
@@ -60,7 +60,7 @@ project_root="$(git rev-parse --show-toplevel)"
   cd "$project_root"
   # Disable exit on error for this subshell, so we can capture the build
   # failure exit status and return our soft-fail exit code
-  set +e
+  set +eo pipefail
   env DO_CHECK=true hab pkg build "$plan"
 )
 status=$?
